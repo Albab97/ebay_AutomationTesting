eeb33f50aff7abab130011c5a4c7dcd72ed93391
Albab97 <albabahmed21@yahoo.com>
2025-09-28 20:02:17 +0530

    change

--- src/test/java/com/test/LoginTests.java
+++ src/test/java/com/test/LoginTests.java
@@ -6,8 +6,10 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.*;
+import org.testng.util.TimeUtils;
 
 import java.time.Duration;
+import java.time.temporal.ChronoUnit;
 import java.util.concurrent.TimeUnit;
 
 public class LoginTests {
@@ -18,21 +20,25 @@ public class LoginTests {
         System.setProperty("webdriver.chrome.driver", "src/main/resources/drivers/chromedriver.exe");
         driver = new ChromeDriver();
         driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        driver.manage().timeouts().implicitlyWait(Duration.of(5, ChronoUnit.SECONDS));
         driver.get("https://www.ebay.com/");
+        driver.findElement(By.partialLinkText("Sign in")).click();
     }
     @Test
-    public void loginValidCredentials(){
-        driver.findElement(By.partialLinkText("Sign in")).click();
+    public void loginValidCredentials() throws InterruptedException {
         String title = driver.getTitle();
         Assert.assertEquals(title,"Sign in or Register | eBay");
-        driver.findElement(By.id("switch-account-link")).click();
+        Thread.sleep(2000);
+//        driver.findElement(By.id("switch-account-anchor")).click();
         driver.findElement(By.id("userid")).sendKeys("albabahmed21@yahoo.com");
-        driver.findElement(By.cssSelector("button[@id='signin-continue-btn']")).click();
-        driver.findElement(By.id("pass")).sendKeys("Jeeshu007@");
-        driver.findElement(By.cssSelector("button[@id='signin-continue-btn']")).click();
-        Alert alert = driver.switchTo().alert();
-        System.out.println(alert.getText());
+//        Thread.sleep(2000);
+        driver.findElement(By.cssSelector("#signin-continue-btn")).click();
+        driver.findElement(By.id("pass")).sendKeys("*********");
+//        Thread.sleep(2000);
+        driver.findElement(By.cssSelector("#sgnBt")).click();
+//        Thread.sleep(2000);
+        String name = driver.findElement(By.xpath("//*[@id=\"gh\"]/nav/div[1]/span[1]/div/button/span/span")).getText();
+        Assert.assertEquals(name,"Albab!");
     }
     @AfterClass
     public void afterTest(){
